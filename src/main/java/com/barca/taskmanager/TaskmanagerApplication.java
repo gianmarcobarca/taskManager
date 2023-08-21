cbbab5bec01dd8f32a3274728e93080ce344002e
Gianmarco Barca <Gianmarco.technology@outlook.com>
2023-08-21 02:49:43 -0400

    refactor

--- src/main/java/com/barca/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/barca/taskmanager/TaskmanagerApplication.java
@@ -2,9 +2,8 @@ package com.barca.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import com.barca.taskmanager.configs.properties.RsaKeysProperties;
+// TODO think about using Log4j2
 
 @SpringBootApplication
 public class TaskmanagerApplication {
@@ -12,6 +11,4 @@ public class TaskmanagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagerApplication.class, args);
 	}
-
-	// TODO think about using Log4j2
 }
